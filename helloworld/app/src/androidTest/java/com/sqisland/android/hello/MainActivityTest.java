3a54514c73ce5cf996ac4caeb2f885dcb924e094
DESKTOP-5VPA9EA\deniz <denizonerorsel@gmail.com>
2018-01-15 18:04:36 +0100

    update testing

--- helloworld/app/src/androidTest/java/com/sqisland/android/hello/MainActivityTest.java
+++ helloworld/app/src/androidTest/java/com/sqisland/android/hello/MainActivityTest.java
@@ -1,8 +1,30 @@
+package com.sqisland.android.hello;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by deniz on 15/01/2018.
  */
-public class MainActivityTest {
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void bye() {
+        onView(withId(R.id.greeting))
+                .check(matches(withText("Hello world!")));
+    }
 }
